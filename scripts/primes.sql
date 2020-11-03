6a1082683ebd48dabb8ce734bab1e445bfad284e
Jack Muskopf <jmuskopf@wustl.edu>
2020-11-03 12:51:10 -0600

    rm old file

--- scripts/primes.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE STREAM "PRIME_NUMBER_STREAM" (number numeric, 
-                                                   isPrime boolean);
--- CREATE OR REPLACE PUMP to insert into output
-CREATE OR REPLACE PUMP "STREAM_PUMP" AS 
-  INSERT INTO "PRIME_NUMBER_STREAM"
-      SELECT "number", "isPrime"
-      FROM   "firehose_in_001"
-      WHERE  "isPrime";
